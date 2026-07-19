4636162c52d1c8f1c3b60b028ba875fcd1eba01f
hirayama971 <68721979+hirayama971@users.noreply.github.com>
2026-07-19 14:59:08 +0900

    Version0.2

---
+++ js/products.js
@@ -0,0 +1,75 @@
+const products = [
+
+    {
+        id:1,
+        name:"マッチ箱風フレークシール",
+        price:350,
+        stock:30,
+        image:"images/match.png"
+    },
+
+    {
+        id:2,
+        name:"アクリルキーホルダー",
+        price:400,
+        stock:30,
+        image:"images/keyholder.png"
+    },
+
+    {
+        id:3,
+        name:"ランダムフレークシール",
+        price:50,
+        stock:100,
+        image:"images/random.png"
+    },
+
+    {
+        id:4,
+        name:"A4クリアファイル",
+        price:500,
+        stock:20,
+        image:"images/a4.png"
+    },
+
+    {
+        id:5,
+        name:"A5クリアファイル",
+        price:400,
+        stock:20,
+        image:"images/a5.png"
+    },
+
+    {
+        id:6,
+        name:"ステッカー",
+        price:150,
+        stock:50,
+        image:"images/sticker.png"
+    },
+
+    {
+        id:7,
+        name:"フレークシール",
+        price:200,
+        stock:40,
+        image:"images/flake.png"
+    },
+
+    {
+        id:8,
+        name:"ポストカード",
+        price:150,
+        stock:50,
+        image:"images/postcard.png"
+    },
+
+    {
+        id:9,
+        name:"ぽち袋",
+        price:100,
+        stock:30,
+        image:"images/pochi.png"
+    }
+
+];
